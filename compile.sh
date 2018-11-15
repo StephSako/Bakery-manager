3f048aa6a26d67d9fb6ef0b7e869d6801f892469
StephSako <stephen.sakovitch@orange.fr>
2018-11-15 10:09:43 +0100

    Scripts

---
+++ compile.sh
@@ -0,0 +1,2 @@
+javac -d logger/bin logger/src/logger/*.java && javac -d restaurant/bin -cp "logger/bin" restaurant/src/restaurant/*.java
+
---
+++ launch.sh
@@ -0,0 +1,2 @@
+java -cp "restaurant/bin:logger/bin" restaurant.Launcher
+
